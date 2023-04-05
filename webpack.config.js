c4ff7f10d80c4056a4ba4d7cc73ee982da2a849f
Denis Natalevich <ndserg@gmail.com>
2023-04-06 00:39:24 +0300

    Доработает сборку webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = (env, argv) => ({
+  mode: argv.mode === 'development' ? 'development' : 'production',
+  devtool: argv.mode === 'development' ? 'source-map' : false,
   entry: './src/main.js',
   output: {
     filename: 'js/bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
-  devtool: argv.mode === 'development' ? 'source-map' : false,
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
@@ -16,30 +18,30 @@ module.exports = (env, argv) => ({
       inject: 'body',
       scriptLoading: 'module'
       }),
-      new FileManagerPlugin({
-        events: {
-          onStart: {
-          delete: ['dist'],
-          },
-          onEnd: {
-            copy: [
-              {source: 'public/**/*',
-              destination: 'dist',
-              globOptions: {
-                ignore: 'public/index.html',
-              },
-            },
-            ],
-          },
+    new FileManagerPlugin({
+      events: {
+        onStart: {
+        delete: ['dist'],
         },
-      }),
+        onEnd: {
+          copy: [
+            {source: 'public/**/*',
+            destination: 'dist',
+            globOptions: {
+              ignore: 'public/index.html',
+            },
+          },
+          ],
+        },
+      },
+    }),
   ],
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'public'),
     },
     open: true,
-    port: 9000,
+    port: 8000,
   },
   watchOptions: {
     ignored: /node_modules/,
